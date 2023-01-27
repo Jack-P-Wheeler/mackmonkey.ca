8fb3d2b85152ef5832fd0155aff560cd6ce976cf
Jack Wheeler <jack.wheeler@rogers.com>
2023-01-27 00:51:29 -0500

    More fixes and minor refactor

--- src/components/DocumentDetailed.jsx
+++ src/components/DocumentDetailed.jsx
@@ -1,26 +1,31 @@
 import { Link } from "react-router-dom"
 import InfoTagSmall from "./InfoTagSmall"
 
-const DocumentDetailed = ({doc, searchData}) => {
+const DocumentDetailed = ({ doc, searchData }) => {
     const formatDate = (time) => {
-        const dateOptions = {dateStyle: "medium", timeStyle: "short"}
+        const dateOptions = { dateStyle: "medium", timeStyle: "short" }
         const date = new Date(time).toLocaleString("en-CA", dateOptions)
         return date
     }
-    
+
+    const documentTitle = () => {
+        const searchIndex = doc.title.toLowerCase().indexOf(searchData.term.toLowerCase())
+        return (<div className="">
+            <span className="text-3xl">{doc.title.slice(0, searchIndex)}</span>
+            <span className="mb-4 font-bold text-3xl">{doc.title.slice(searchIndex, searchIndex + searchData.term.length)}</span>
+            <span className="text-3xl">{doc.title.slice(searchIndex + searchData.term.length)}</span>
+        </div>)
+    }
+
     return (
         <Link key={doc.id} to={"/doc/" + doc.id} className="border p-4 rounded-md my-4 mr-4 shadow-lg flex flex-col h-64 hover:shadow-xl transition-all">
             {doc.title.toLowerCase().includes(searchData.term.toLowerCase())
-                ? <div className="">
-                    <span className="text-3xl">{doc.title.slice(0, doc.title.toLowerCase().indexOf(searchData.term.toLowerCase()))}</span>
-                    <span className="mb-4 font-bold text-3xl">{doc.title.slice(doc.title.toLowerCase().indexOf(searchData.term.toLowerCase()), searchData.term.length)}</span>
-                    <span className="text-3xl">{doc.title.slice(doc.title.toLowerCase().indexOf(searchData.term.toLowerCase()) + searchData.term.length)}</span>
-                </div>
+                ? documentTitle()
                 : <p className="text-3xl">{doc.title}</p>}
 
             {doc.expand.category
-            ? doc.expand.category.map((cat) => <InfoTagSmall color={cat.color} name={cat.name}/>)
-            : null}
+                ? doc.expand.category.map((cat) => <InfoTagSmall color={cat.color} name={cat.name} />)
+                : null}
 
             <p className="italic mt-auto">Last updated: {formatDate(doc.updated)}</p>
 
